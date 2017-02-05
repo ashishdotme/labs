37dd8abbc24ab7c3a513210fb439f0e5a47d70b2
Ashish Patel <ashishsushilpatel@gmail.com>
2017-02-05 15:55:37 +0530

    Completed random quote project

--- freecodecamp/random-quote-machine/src/_scripts/app.js
+++ freecodecamp/random-quote-machine/src/_scripts/app.js
@@ -1,7 +1,47 @@
-(function($) {
-  $(document).ready(function () {
-    $('.nav-toggle').click(function () {
-      $('.nav-menu').toggleClass('is-active');
+var quote = '';
+var author = '';
+
+var getQuote = function() {
+    $.ajax({
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Key", "2i46xuS3BrmshMZJMG5DtSTCiwYZp1CY0btjsn0uwuEf5N8TP9")
+        },
+        success: function(json) {
+            //process the JSON data etc
+            var obj = $.parseJSON(json);
+            quote = obj.quote;
+            author = obj.author;
+            $('.title').text(quote);
+            $('.subtitle').text('- ' + author);
+            var tweet = 'https://twitter.com/intent/tweet?text=' + quote + ' - ' + author;
+            $('#tweetquote').attr("href",tweet);
+        }
+    })
+}
+
+$(document).ready(function() {
+
+    $('.nav-toggle').click(function() {
+        $('.nav-menu').toggleClass('is-active');
     });
-  });
-})(jQuery);
+
+    $.LoadingOverlaySetup({
+        color: "rgba(0, 0, 0, 0.8)",
+        image: "../img/loader.svg"
+    });
+
+    $(document).ajaxSend(function(event, jqxhr, settings) {
+        $.LoadingOverlay("show");
+    });
+
+    $(document).ajaxComplete(function(event, jqxhr, settings) {
+        $.LoadingOverlay("hide");
+    });
+
+    getQuote();
+
+    $('#newquote').click(function() {
+      getQuote();
+    })
+});
