738d6b1157d327214a253968a75465835d7f0e7b
Ashish Patel <ashish.patel@nitorinfotech.com>
2017-02-17 18:51:26 +0530

    Added weather icons

--- freecodecamp/local-weather/src/_scripts/app.js
+++ freecodecamp/local-weather/src/_scripts/app.js
@@ -7,19 +7,22 @@
       $('.nav-menu').toggleClass('is-active');
     });
 
-    $.getJSON("http://freegeoip.net/json/", function(data) {
-      var city = data.city
-      var country = data.country_name;
-      var ip = data.ip;
-      var long = data.longitude;
-      var lat = data.latitude;
-      getWeather(long, lat, "metric", city, country);
-    });
+    function getLocation() {
+      if ("geolocation" in navigator){ //check geolocation available 
+          //try to get user current location using getCurrentPosition() method
+          navigator.geolocation.getCurrentPosition(function(position){ 
+                  console.log(position.coords.latitude);
+                  console.log(position.coords.longitude);
+                  getWeather(position.coords.longitude, position.coords.latitude, "metric", "Pune", "India");
+              });
+      } else{
+          console.log("Browser doesn't support geolocation!");
+      }
+    }
+
+     getLocation();
 
     function getWeather(long, lat, unit, city, country) {
-      console.log(long);
-      console.log(lat);
-      console.log(unit);
       var url = "http://api.openweathermap.org/data/2.5/weather?" +
         "lat=" + lat +
         "&lon=" + long +
@@ -27,10 +30,12 @@
         "&appid=" + "54dbbad81431def0526364a71dcdf1a1";
       console.log(url);
       $.getJSON(url, function(data) {
-        console.log(data);
         $('.temp').text(data.main.temp + "°");
-        $('#city').text(city+ ', '+ country);
+        $('#city').text(data.name+ ', '+ data.sys.country);
         $('#main').text(data.weather[0].main);
+        $('#precipitation').text(data.main.pressure+ ' hPa');
+        $('#humidity').text(data.main.humidity+'%');
+        $('#wind').text(data.wind.speed+" km/h");
       });
     }
 
