90736b58a2c4e6c198899ded44e4797e809d4e65
Ashish Patel <ashish.patel@nitorinfotech.com>
2017-02-19 02:20:36 +0530

    Completed local weather project

--- freecodecamp/local-weather/src/_scripts/app.js
+++ freecodecamp/local-weather/src/_scripts/app.js
@@ -1,5 +1,8 @@
 (function($) {
   $(document).ready(function() {
+    //Variables
+    var lat = "";
+    var long = "";
 
     $('#date').text($.datepicker.formatDate('M d yy', new Date()));
 
@@ -7,26 +10,38 @@
       $('.nav-menu').toggleClass('is-active');
     });
 
+    $.LoadingOverlaySetup({
+        color: "rgba(0, 0, 0, 0.8)",
+        image: "img/loader.svg"
+    });
+
+    $(document).ajaxSend(function(event, jqxhr, settings) {
+        $.LoadingOverlay("show");
+    });
+
+    $(document).ajaxComplete(function(event, jqxhr, settings) {
+        $.LoadingOverlay("hide");
+    });
+
     function getLocation() {
       if ("geolocation" in navigator){ //check geolocation available 
-          //try to get user current location using getCurrentPosition() method
           navigator.geolocation.getCurrentPosition(function(position){ 
-                  console.log(position.coords.latitude);
-                  console.log(position.coords.longitude);
-                  getWeather(position.coords.longitude, position.coords.latitude, "metric", "Pune", "India");
+                  lat = position.coords.latitude;
+                  long = position.coords.longitude;
+                  getWeatherInCelsius(long, lat);
               });
       } else{
           console.log("Browser doesn't support geolocation!");
       }
     }
 
-     getLocation();
+    getLocation();
 
-    function getWeather(long, lat, unit, city, country) {
+    function getWeatherInCelsius(long, lat) {
       var url = "http://api.openweathermap.org/data/2.5/weather?" +
         "lat=" + lat +
         "&lon=" + long +
-        "&units=" + unit +
+        "&units=metric" +
         "&appid=" + "54dbbad81431def0526364a71dcdf1a1";
       console.log(url);
       $.getJSON(url, function(data) {
@@ -39,5 +54,37 @@
       });
     }
 
+    function getWeatherInFarenheit(long, lat) {
+      var url = "http://api.openweathermap.org/data/2.5/weather?" +
+        "lat=" + lat +
+        "&lon=" + long +
+        "&units=imperial" +
+        "&appid=" + "54dbbad81431def0526364a71dcdf1a1";
+      console.log(url);
+      $.getJSON(url, function(data) {
+        $('.temp').text(data.main.temp + " F");
+        $('#city').text(data.name+ ', '+ data.sys.country);
+        $('#main').text(data.weather[0].main);
+        $('#precipitation').text(data.main.pressure+ ' hPa');
+        $('#humidity').text(data.main.humidity+'%');
+        $('#wind').text(data.wind.speed+" mph");
+      });
+    }
+
+    $('#farenheit').click(function(){
+      console.log("Ashish rules the world");
+      $("#farenheit-active").addClass("is-active");
+      $("#celsius-active").removeClass("is-active");
+      getWeatherInFarenheit(long, lat);
+    });
+
+    $('#celsius').click(function(){
+      console.log("Ashish rules the world");
+      $("#celsius-active").addClass("is-active");
+      $("#farenheit-active").removeClass("is-active");
+      getWeatherInCelsius(long, lat);
+    });
+
   });
+
 })(jQuery);
